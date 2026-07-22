6e0b3913635f38e8c2e41c2e7c0dae6a4500e9f1
Adithya Ram <adithya.ram@nybinfotech.com>
2026-07-22 09:19:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// Virtual DOM
+// The Virtual DOM (VDOM) is a lightweight copy of the real DOM. 
+// It is a JavaScript object that represents the structure of your web page.
+
+// How it Works
+// The UI is rendered and a Virtual DOM is created.
+// When data changes, a new Virtual DOM is created.
+
+// Library
+// A library is a collection of pre-written code that you can use in your program.
+//  You are in control and decide when to call the library.
+
+// Framework
+// A framework provides a complete structure for building an application. 
+// The framework is in control and calls your code at the appropriate time.
+
+// React
+// React is a free, open-source JavaScript library used to build user interfaces (UI), 
+// especially for Single Page Applications (SPAs). 
+// It was developed by Meta (Facebook).
+
+
+// Components 
+// Component is a small building block of a web page
+// components make our code cleaner,reusable and easy to manage
+// in components we write code using javascript and html like code called JSX
+
+
+// Vanilla Javascript is different from react js because react makes the code 
+// look cleaner and easier and make dynamic user interfaces. 
+
+// What is VITE ?
+// is a modern frontend build tool that helps developers create web applications quickly. 
+// It provides a fast development server and an optimized production build.
+
+// Why VITE is Used?
+// Before Vite, developers often used tools like Create React App (CRA) or Webpack. As projects grew, 
+// starting the development server and rebuilding the application became slower.
